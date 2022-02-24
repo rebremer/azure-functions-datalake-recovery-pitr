66353b96492f5ff2865fe6c6b147550623c21833
Rene Bremer <rebremer@microsoft.com>
2022-02-24 15:17:23 +0100

    fixed datetime comparison

--- ActivityPointInTimeRecovery/__init__.py
+++ ActivityPointInTimeRecovery/__init__.py
@@ -9,7 +9,8 @@
 # https://docs.microsoft.com/en-us/azure/azure-functions/functions-create-storage-blob-triggered-function
 
 import logging
-from datetime import datetime, timedelta
+from datetime import datetime
+from dateutil.parser import parse
 from azure.identity import DefaultAzureCredential
 from azure.storage.blob import (
     BlobServiceClient
@@ -22,6 +23,7 @@ def main(jsoninput: str) -> str:
     file_system = jsoninput["file_system"]
     folder_name = jsoninput["folder_name"]
     restore_date = jsoninput["restore_date"]
+    restore_date_object = parse(restore_date) # string to datetime object
 
     # In case no MI is used, add AZURE_TENANT_ID, AZURE_CLIENT_ID and AZURE_CLIENT_SECRET to environment variables
     token_credential = DefaultAzureCredential()
@@ -68,12 +70,12 @@ def main(jsoninput: str) -> str:
                         snapshot_last_modified_exists = True
 
                     # 2. Determine is snapshot is most recent before restore date
-                    if str(blob_snapshot.last_modified) < restore_date:
+                    if blob_snapshot.last_modified < restore_date_object:
                         # 1a. snapshot is before restore date
                         if restore_snapshot == None:
                             # 1a1. no restore_snapshot yet => current snapshot becomes restore_snapshot candidate
                             restore_snapshot = blob_snapshot
-                        elif str(blob_snapshot.last_modified) > str(restore_snapshot.last_modified):
+                        elif blob_snapshot.last_modified > restore_snapshot.last_modified:
                             # 1a2. current snapshot is closer to restore date than previous snapshot => current snapshot becomes restore_snapshot candidate
                             restore_snapshot = blob_snapshot
 
@@ -83,15 +85,18 @@ def main(jsoninput: str) -> str:
                 metadata={'snapshot_before_restore_date':'{}'.format(restore_date)}
                 blob_client.create_snapshot(metadata=metadata)
 
+            print("last_modified: " + str(blob_client.get_blob_properties()['last_modified'] ) + ", restore_date: " + restore_date)
+
             # Check scenarios
-            if str(blob_client.get_blob_properties()['last_modified']) < restore_date:
+            if blob_client.get_blob_properties()['last_modified'] < restore_date_object:
+
                 # scenario 1: last_modified < restore_date 
                 # => no need to restore
-                print(str(blob.name) + " last change was before restore date, do nothing")
-            elif restore_date < str(blob_client.get_blob_properties()['creation_time']): 
+                print("Scenario 1: " + str(blob.name) + " last change was before restore date, do nothing")
+            elif restore_date_object < blob_client.get_blob_properties()['creation_time']: 
                 # scenario 2: restore_date < creation_time 
                 # => soft delete file
-                print(str(blob.name) + " restore date is before creation time, soft delete")  
+                print("Scenario 2: " + str(blob.name) + " restore date is before creation time, soft delete")  
                 blob_client.delete_blob(delete_snapshots="include")          
             elif restore_snapshot != None:
                 # Scenario 3: creation_time < restore_date < last_modified && snapshot before last_modified is found
@@ -99,13 +104,15 @@ def main(jsoninput: str) -> str:
                 blob_client.start_copy_from_url("{}://{}.blob.core.windows.net/{}/{}?snapshot={}".format("https", storage_account_name, file_system, restore_snapshot.name, restore_snapshot.snapshot))
                 metadata={'restored_snapshot':'{}'.format(restore_snapshot.snapshot)}
                 blob_client.set_blob_metadata(metadata=metadata)
+                print("Scenario 3: Restore snapshot " + str(restore_snapshot.snapshot))
             else:
                 # Scenario 4: creation_time < restore_date < last_modified && snapshot before last_modified is NOT found
                 # => Error state, no way to resolve, requires analysis of content of blob what shall be done. 
                 # => Do nothing, log error message
-                blob_client = blob_service_client.get_blob_client(container=file_system, blob="_log/exception/" + str(restore_date) + ".txt")
-                blob_client.append_block(data="Scenario 4: " + str(blob.name) + str(restore_date) + "_" + str(datetime.now()),overwrite=True)
+                blob_client = blob_service_client.get_blob_client(container=file_system, blob="_log/exception/" + restore_date + ".txt")
+                blob_client.append_block(data="Scenario 4: " + str(blob.name) + restore_date + "_" + str(datetime.now()),overwrite=True)
 
+                print("Scenario 4: error")
         else:
             print (blob.name + ' is a directory')
             if blob.name + "/" != folder_name:
@@ -114,11 +121,11 @@ def main(jsoninput: str) -> str:
         i+=1
 
         blob_client = blob_service_client.get_blob_client(container=file_system, blob="_log/" + folder_name + "restore-correlation" + str(restore_date) + ".txt")
-        blob_client.upload_blob(data="correlation_" + str(restore_date) + "_" + str(datetime.now()),overwrite=True)
+        blob_client.upload_blob(data="correlation_" + restore_date + "_" + str(datetime.now()),overwrite=True)
 
     # delete empty folders that were created before restore date
     for sub_folder in sub_folder_list:
-        if str(sub_folder.last_modified) < restore_date:
+        if sub_folder.last_modified < restore_date_object:
             # folder was created before restore date, don't do anyting
             print("folder was created before restore date, don't do anyting")
         else:
@@ -130,7 +137,7 @@ def main(jsoninput: str) -> str:
                     # subfolder contains blob, don't delete subfolder, break
                     no_blobs_in_sub_folder = False
                     break
-                elif str(sub_folder_blob.last_modified) < restore_date: 
+                elif sub_folder_blob.last_modified < restore_date_object: 
                     # subfolder contains another subfolder that was created before restore date, don't delete subfolder, break
                     no_blobs_in_sub_folder = False
                     break
